369482c081e10dcb1d42422852dac3a79a0e308d
Manoel Ribeiro <ribeiromanoel2003@gmail.com>
2026-09-04 15:08:58 -0300

    liberado cors para localhost e retirado host 0.0.0.0

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,18 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(
-    new ValidationPipe(
-      {
-        whitelist: true, // remove chaves que não estão no DTO
-        forbidNonWhitelisted: true, // levantar erro quando a chave não existir
-        transform: false, // tenta transformar os tipos de dados de param e dtos
-      }
-    )
-  )
+  app.enableCors({
+    origin: 'http://localhost:4200',
+  });
 
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // remove chaves que não estão no DTO
+      forbidNonWhitelisted: true, // levantar erro quando a chave não existir
+      transform: false, // tenta transformar os tipos de dados de param e dtos
+    }),
+  );
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
